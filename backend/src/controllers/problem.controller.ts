035d50c52b8ecf8076e1c30effdae68b31c30800
saurabh007007 <loverboybhol143@gmail.com>
2025-06-26 01:13:45 +0530

    Feat:Created problem routes half done

--- backend/src/controllers/problem.controller.ts
+++ backend/src/controllers/problem.controller.ts
@@ -1,44 +1,84 @@
+import { Request, Response } from "express";
+import { prisma } from "../libs/PrismaDb";
+import { getJudge0LanguageId, pollBatchResults, submitBatch } from "../libs/juge0.lib";
 
-import { Request,Response } from "express"
-import {prisma} from "../libs/PrismaDb"
-import { getJudge0LanguageId } from "../libs/juge0.lib";
+export const createProblem = async (
+    req: Request,
+    res: Response
+): Promise<any> => {
+    //  get all the req data from body
+    const {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        refrenceSolution,
+    } = req.body;
 
-export const createProblem= async (req: Request, res: Response): Promise<any> => {
-//  get all the req data from body 
-const{title,description,difficulty,tags,examples,constraints,testcases,codeSnippets,refrenceSolution} =req.body;
-
-if(req.user?.role !== "ADMIN") {
-    return res.status(403).json({
-        message: "You are not authorized to create a problem.",
-        success: false
-        
-    });
-}
+    if (req.user?.role !== "ADMIN") {
+        return res.status(403).json({
+            message: "You are not authorized to create a problem.",
+            success: false,
+        });
+    }
     try {
+        for (const [langauge, solutionCode] of Object.entries(
+            refrenceSolution || {}
+        )) {
+            const languageId = getJudge0LanguageId(langauge);
+            if (!languageId) {
+                return res.status(400).json({
+                    message: `Unsupported language: ${langauge}`,
+                    success: false,
+                });
+            }
+            //@ts-ignore
+            const submissions = testcases.map(({ input, output }) => ({
+                source_code: solutionCode,
+                language_id: languageId,
+                stdin: input,
+                expected_output: output,
+            }));
+
+            const submissionResults= await submitBatch(submissions)
         
-    for (const [langauge,solutionCode] of Object.entries(refrenceSolution || {})){
-        const languageId =getJudge0LanguageId(langauge);
-        if (!languageId) {
-            return res.status(400).json({
-                message: `Unsupported language: ${langauge}`,
-                success: false
-            });
+            const tokens=submissionResults.map((res:any)=>res.token);
+
+            const results= await pollBatchResults(tokens);
+
         }
-    }
-    } catch (error) {
-        
-    }
-
-}
-export const getProblems= async (req: Request, res: Response): Promise<any> => {}
 
 
-export const getProblemById =async (req: Request, res: Response): Promise<any> => {}
+    } catch (error) { }
+};
 
 
 
-export const updateProblemById =async (req: Request, res: Response): Promise<any> => {}
+export const getProblems = async (
+    req: Request,
+    res: Response
+): Promise<any> => { };
 
-export const deleteProblemById =async (req: Request, res: Response): Promise<any> => {}
+export const getProblemById = async (
+    req: Request,
+    res: Response
+): Promise<any> => { };
 
-export const getAllProblemSolvedByUser= async (req: Request, res: Response): Promise<any> => {}
+export const updateProblemById = async (
+    req: Request,
+    res: Response
+): Promise<any> => { };
+
+export const deleteProblemById = async (
+    req: Request,
+    res: Response
+): Promise<any> => { };
+
+export const getAllProblemSolvedByUser = async (
+    req: Request,
+    res: Response
+): Promise<any> => { };
--- backend/src/libs/juge0.lib.ts
+++ backend/src/libs/juge0.lib.ts
@@ -1,10 +1,39 @@
+import axios from "axios";
+
 export const getJudge0LanguageId = (language: string): number | null => {
-    const languageMap: Record<string, number> = {
-      python: 71,
-      java: 62,
-      javascript: 63,
-    };
-  
-    return languageMap[language.toLowerCase()] ?? null;
+  const languageMap: Record<string, number> = {
+    python: 71,
+    java: 62,
+    javascript: 63,
   };
-  
+
+  return languageMap[language.toLowerCase()] ?? null;
+};
+
+export const submitBatch= async (submissions: any[]): Promise<any> => {
+    const {data}=await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,{submissions})
+    console.log("Results:",data)
+    return data;
+}
+
+
+const sleep  = (ms:any)=> new Promise((resolve)=> setTimeout(resolve , ms))
+
+export const pollBatchResults= async (tokens:any)=>{
+    while(true){
+        const {data}=await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`,{
+            params:{
+                tokens:tokens.join(","),
+                base64_encoded:false,
+                
+            }
+       })
+       const results=data.submissions;
+       const isAllDone=results.every((result:any)=> result.status.id!==1 && result.status.id!==2 )
+       if(isAllDone) return results;
+
+       await sleep(1000)
+        
+    }
+
+}
