86bae48a1b9bdbb39145e0e1aeda6d1085d9cb50
saurabh007007 <loverboybhol143@gmail.com>
2025-06-25 23:57:53 +0530

    Feat:Added the boilerplate for the problems

---
+++ backend/src/controllers/problem.controller.ts
@@ -0,0 +1,18 @@
+
+import { Request,Response } from "express"
+
+export const createProblem= async (req: Request, res: Response): Promise<any> => {
+
+}
+export const getProblems= async (req: Request, res: Response): Promise<any> => {}
+
+
+export const getProblemById =async (req: Request, res: Response): Promise<any> => {}
+
+
+
+export const updateProblemById =async (req: Request, res: Response): Promise<any> => {}
+
+export const deleteProblemById =async (req: Request, res: Response): Promise<any> => {}
+
+export const getAllProblemSolvedByUser= async (req: Request, res: Response): Promise<any> => {}
--- backend/src/routes/problem.routes.ts
+++ backend/src/routes/problem.routes.ts
@@ -1,9 +1,30 @@
 import express from "express"
+import { authMiddeware, isAdmin } from "../middleware/auth.middleware";
+import { createProblem,getProblemById,getProblems ,getAllProblemSolvedByUser,updateProblemById,deleteProblemById} from "../controllers/problem.controller";
 
 const problemRouter = express.Router()
 
 
+//to create the problesm 
+problemRouter.post("/create-problem",authMiddeware,isAdmin,createProblem)
+//to get all problems
 
+problemRouter.get("/get-all-problems",authMiddeware,getProblems)
+
+//to get a single problem by id\
+problemRouter.get("/get-problem/:id",authMiddeware,getProblemById)
+
+//update problems 
+
+problemRouter.put("/update-problem/:id",authMiddeware,isAdmin,updateProblemById)
+
+//delete problems 
+
+problemRouter.delete("/delete-problem/:id",authMiddeware,isAdmin,deleteProblemById)
+
+//get all solved problems 
+
+problemRouter.get("/get-solved-problems", authMiddeware, getAllProblemSolvedByUser);
 
 
 export default problemRouter;
