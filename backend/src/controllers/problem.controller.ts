4969f79775578b45dd8380d9f1d6e9e4998cae16
saurabh007007 <loverboybhol143@gmail.com>
2025-07-06 17:28:22 +0530

    added the problem here

--- backend/src/controllers/problem.controller.ts
+++ backend/src/controllers/problem.controller.ts
@@ -104,7 +104,31 @@ export const createProblem = async (
 export const getProblems = async (
     req: Request,
     res: Response
-): Promise<any> => { };
+): Promise<any> => { 
+    try {
+        const problems= await prisma.problem.findMany();
+        if(!problems || problems.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: "No problems found"
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            problems: problems,
+            message: "Problems fetched successfully"
+        })
+
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Error in fetching problems",
+            error:error
+        })
+        
+    }
+};
 
 export const getProblemById = async (
     req: Request,
