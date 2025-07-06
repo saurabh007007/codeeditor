a15127db699476478233e6b548ea718ad3e18f03
saurabh007007 <loverboybhol143@gmail.com>
2025-07-06 22:00:32 +0530

    delete problem by id and changes the mode of running the app

--- backend/src/controllers/problem.controller.ts
+++ backend/src/controllers/problem.controller.ts
@@ -281,7 +281,44 @@ export const updateProblemById = async (
 export const deleteProblemById = async (
     req: Request,
     res: Response
-): Promise<any> => { };
+): Promise<any> => {
+    const {id}=req.params;
+
+    if(req.user?.role !== 'ADMIN'){
+        return res.status(403).json({
+            success:false,
+            message:"You are not authorized to delete this problem"
+        })
+    }
+    if(!id){
+        return res.status(400).json({
+            success:false,
+            message:"Problem ID is required"
+        })
+    }
+    try {
+        const deletedProblem = await prisma.problem.delete({
+            where:{
+                id
+            }
+        })
+        return res.status(200).json({
+            success:true,
+            message:"Problem deleted successfully",
+            problem:deletedProblem
+        })
+
+        
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            success:false,
+            message:"Error in deleting problem",
+            error:error
+        })
+        
+    }
+ };
 
 export const getAllProblemSolvedByUser = async (
     req: Request,
