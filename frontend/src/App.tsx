661c7875a4707713a08747ca1b036c2fa9d94f53
saurabh007007 <loverboybhol143@gmail.com>
2025-07-14 22:01:51 +0530

    Updated the CHANGELOG.md file for the MacOS setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,33 @@
-import {Routes,Route, Navigate} from 'react-router-dom'
-import { LandingPage } from './pages/LandingPage'
-import { LoginPage } from './pages/LoginPage'
-import { SignupPage } from './pages/SignupPage'
-import { HomePage } from './pages/HomePage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
-let authUser=null;
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    
-    <Routes>
-      <Route path='/' element={<LandingPage/>} />
-      <Route path='/login' 
-      element={!authUser ? <LoginPage/> : <Navigate to={"/home"} />} 
-      />
-      <Route path='/signup' 
-      element={!authUser?<SignupPage/>:<Navigate to={"/home"}/>} 
-      /> 
-      <Route path="/home" element={!authUser?<HomePage/>:<Navigate to={"/"}/>
-    }/>
-    </Routes>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from "sonner"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Toaster richColors position="bottom-right"/>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
