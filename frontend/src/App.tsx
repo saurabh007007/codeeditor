a1786d2acdd454d796c9a5b64bf51e456d8d45ff
saurabh007007 <loverboybhol143@gmail.com>
2025-07-16 01:07:11 +0530

    added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+import { Login } from "./components/AuthComponents/Login"
+import { Home } from "./pages/Home"
+import { MainPage } from "./pages/MainPage"
+
+
 
 function App() {
+  let authUser=null
 
   return (
     <>
-    <div className='bg-red-300 items-center flex justify-center h-screen text-9xl text-red-400' > hi there </div>
+      <div className="flex flex-col items-center justify-start">
+
+
+        <Routes>
+          <Route path="/" element={<Home/> }/>
+          <Route path="/home" element={<MainPage/>}/>
+          <Route path="/login" element={!authUser?<Login /> : <Navigate to={ "/home" } />} />
+          
+        </Routes>
+      </div>
     </>
   )
 }
