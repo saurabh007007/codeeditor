7c4a66e126c04b47da1c9374cc88d19b3bebf362
saurabh007007 <loverboybhol143@gmail.com>
2025-07-16 00:38:48 +0530

    addewd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <div className='bg-red-300 items-center flex justify-center h-screen' > hi there </div>
+    <div className='bg-red-300 items-center flex justify-center h-screen text-9xl text-red-400' > hi there </div>
     </>
   )
 }
